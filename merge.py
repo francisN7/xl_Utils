d136bc4bda2bbe5ceba9236a4ce2390baa26d144
francis <francielfrancasegundo@gmail.com>
2025-06-27 13:01:20 -0300

    Editado a lógica de merge.py
    Todo o código foi refatorado como classe.
    Como TkInter era usado somente para selecionar as planilhas, foi substituído por file_picker_py.

--- merge.py
+++ merge.py
@@ -1,45 +1,70 @@
 import pandas as pd
-from tkinter import Tk, filedialog
+from file_picker_py import pick_files_blocking
+from pathlib import Path
 
-#Selecionar os arquivos:
-def select_files():
-    Tk().withdraw()  # Oculta a janela principal do Tkinter
-    file_paths = filedialog.askopenfilenames(
-        title="Selecione os arquivos Excel",
-        filetypes=[("Arquivos Excel", "*.xlsx *.xls")]
-    )
-    return file_paths
+class Merger():
+    def __init__(self):
+        self.__file_paths: list[Path] = []
+        self.__dataframes: list[pd.DataFrame] = []
+        self.__combined_df: pd.DataFrame = None
 
-#Unificar as planilhas:
-def unify_excel_files():
-    file_paths = select_files()
-    if not file_paths:
-        print("Nenhum arquivo selecionado.")
-        return
-    
-    dataframes = []
-
-    for file in file_paths:
+    def run(self) -> None:
         try:
-            df = pd.read_excel(file)
-            dataframes.append(df)
+            self.__select_files()
         except Exception as e:
-            print(f"Erro ao ler {file}: {e}")
+            print(f"Erro ao selecionar arquivos: {e}")
 
-    if dataframes:
-        combined_df = pd.concat(dataframes, ignore_index=True)  # Combina os DataFrames
-        save_path = filedialog.asksaveasfilename(
-            title="Salvar arquivo unificado",
-            defaultextension=".xlsx",
-            filetypes=[("Arquivo Excel", "*.xlsx")]
-        )
-        if save_path:
-            combined_df.to_excel(save_path, index=False)
-            print(f"Planilhas unificadas e salvas em: {save_path}")
+        try:
+            self.__extract_dataframes()
+        except Exception as e:
+            print(f"Erro ao ler planilhas: {e}")
+
+        try:
+            self.__treat_dataframes()
+        except Exception as e:
+            print(f"Erro ao tratar dataframes extraídos: {e}")
+
+        try:
+            self.__merge_dataframes()
+        except Exception as e:
+            print(f"Erro ao mesclar planilhas: {e}")
+
+        try:
+            self.__save()
+        except Exception as e:
+            print(f"Erro ao salvar planilha: {e}")
+
+
+    def __select_files(self) -> None:
+        for path in pick_files_blocking():
+            self.__file_paths.append(Path(path).absolute())
+
+        filen_n = len(self.__file_paths)
+        if filen_n == 1:
+            print("1 arquivo selecionado.")
         else:
-            print("Operação cancelada. Nenhum arquivo foi salvo.")
-    else:
-        print("Nenhum dado foi combinado.")
+            print(f"{filen_n} arquivos selecionados.")
+
+    def __extract_dataframes(self) -> None:
+        for file in self.__file_paths:
+            df = pd.read_excel(file)
+            self.__dataframes.append(df)
+
+    def __treat_dataframes(self) -> None:
+        self.__dataframes = [
+            df for df in self.__dataframes
+            if not df.empty and not df.isna().all().all()
+        ]
+
+    def __merge_dataframes(self) -> None:
+        self.__combined_df = pd.concat(self.__dataframes, ignore_index=True, sort=False)
+
+    def __save(self) -> None:
+        save_path = self.__file_paths[0].parent.joinpath("Merge.xlsx")
+        self.__combined_df.to_excel(save_path, index=False)
+        print(f"Planilhas unificadas e salvas em: {save_path}")
+
 
 if __name__ == "__main__":
-    unify_excel_files()
+    merger = Merger()
+    merger.run()
