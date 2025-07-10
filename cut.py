0ced810eaae96d6d78491c9657bafe62914ae184
Francis <francielfrancasegundo@gmail.com>
2025-07-10 12:01:08 -0300

    Leve alteração nos comentários

--- cut.py
+++ cut.py
@@ -5,74 +5,82 @@ from pathlib import Path
 import pandas as pd
 import os
 
-# ______Cortador______:
-class Cutter():
-    def __init__(self):
-        self.__file_path:Path = None # Diretório do arquivo.
-        self.__files: list [pd.DataFrame] = [] # Dataframes recortados.
-        self.__df: pd.DataFrame = None # Dataframe original.
 
-    def cut(self) -> None: # Faz os recortes
-        self.__clear() # Limpa o terminal
+# ______Cortador______:
+class Cutter:
+    def __init__(self):
+        self.__file_path: Path = None  # Diretório do arquivo.
+        self.__files: list[pd.DataFrame] = []  # Dataframes recortados.
+        self.__df: pd.DataFrame = None  # Dataframe original.
+
+    def cut(self) -> None:  # Faz os recortes
+        self.__clear()  # Limpa o terminal
         print(":__________Recortar Planilhas__________:\n\n")
-        self.__file_path = Path(pick_file_blocking()).absolute() # Define o diretório do arquivo
-        self.__files = [] # Limpa a lista de arquivos
+        self.__file_path = Path(
+            pick_file_blocking()
+        ).absolute()  # Define o diretório do arquivo
+        self.__files = []  # Limpa a lista de arquivos
         print("Carregando a planilha selecionada...")
-        self.__load_df() # Carrega o dataframe original
-        lines = self.__lines() # Linhas por arquivo
-        n = ceil(len(self.__df) / lines) # Número de arquivos necessários
+        self.__load_df()  # Carrega o dataframe original
+        lines = self.__lines()  # Linhas por arquivo
+        n = ceil(len(self.__df) / lines)  # Número de arquivos necessários
         print("Recortando...")
-        for i in range(n): # Para cada arquivo
-            inicio = i * lines # Linha inicial
-            fim = (i + 1) * lines # Linha final
-            self.__files.append(self.__df.iloc[inicio:fim]) # Adiciona a lista de Dataframes
+        for i in range(n):  # Para cada arquivo
+            inicio = i * lines  # Linha inicial
+            fim = (i + 1) * lines  # Linha final
+            self.__files.append(
+                self.__df.iloc[inicio:fim]
+            )  # Adiciona a lista de Dataframes
         self.__save_files()
 
-    def __load_df(self) -> None: # Carrega o Dataframe original
+    def __load_df(self) -> None:  # Carrega o Dataframe original
         self.__df = pd.read_excel(self.__file_path)
 
-    def __save_files(self) -> None: # Salva os recortes
-        n = 1 # Contador de arquivos
-        output = Path(f"{self.__file_path.parent.as_posix()}/output/") # Diretório dos recortes
-        if not Path.exists(output): # Se a pasta não existir
-            Path.mkdir(output) # Cria a pasta
-        for dataframe in self.__files: # Para cada dataframe
+    def __save_files(self) -> None:  # Salva os recortes
+        n = 1  # Contador de arquivos
+        output = Path(
+            f"{self.__file_path.parent.as_posix()}/output/"
+        )  # Diretório dos recortes
+        if not Path.exists(output):  # Se a pasta não existir
+            Path.mkdir(output)  # Cria a pasta
+        for dataframe in self.__files:  # Para cada dataframe
             self.__clear()
             print(":__________Recortar Planilhas__________:\n\n")
-            new_path = Path.joinpath(output, f"{self.__file_path.stem}_Recorte_{n}.xlsx") # Gera o nome do arquivo
-            n += 1 # Aumenta o contador
+            new_path = Path.joinpath(
+                output, f"{self.__file_path.stem}_Recorte_{n}.xlsx"
+            )  # Gera o nome do arquivo
+            n += 1  # Aumenta o contador
             print(f"Salvando...\n{new_path}")
-            dataframe.to_excel(new_path, index=False, engine="xlsxwriter") # Salva
+            dataframe.to_excel(new_path, index=False, engine="xlsxwriter")  # Salva
 
-    def __clear(self) -> None: # Limpa o terminal
+    def __clear(self) -> None:  # Limpa o terminal
         # Para Windows
-        if os.name == 'nt':
-            os.system('cls')
+        if os.name == "nt":
+            os.system("cls")
         # Para Mac e Linux
         else:
-            os.system('clear')
+            os.system("clear")
 
-    def __lines(self) -> int: # Retorna a quantidade de linhas
-        lines = 0 # Quantidade de linhas
-        while lines <= 0: # Enquanto for menor ou igual a zero
-            self.__clear() # Limpa o terminal
+    def __lines(self) -> int:  # Retorna a quantidade de linhas
+        lines = 0  # Quantidade de linhas
+        while lines <= 0:  # Enquanto for menor ou igual a zero
+            self.__clear()  # Limpa o terminal
             print(":__________Recortar Planilhas__________:\n\n")
-            try: # Tenta receber um inteiro
+            try:  # Tenta receber um inteiro
                 question = "Quantas linhas por arquivo?"
-                lines = int(input(
-                    f"{"="*len(question)}\n" \
-                    f"{question}\n" \
-                    f"{"="*len(question)}\n"
-                ))
-            except Exception as e: # Se a entrada por errada!
+                lines = int(
+                    input(f"{'=' * len(question)}\n{question}\n{'=' * len(question)}\n")
+                )
+            except Exception as e:  # Se a entrada for errada!  # noqa: F841
                 input(
                     "Por favor, informe um número válido!! \
                     \nPressione Enter para continuar..."
                 )
-        self.__clear() # Limpa o terminal
+        self.__clear()  # Limpa o terminal
         print(":__________Recortar Planilhas__________:\n\n")
-        return lines # Retorna a quantidade de linhas
-    
+        return lines  # Retorna a quantidade de linhas
+
+
 if __name__ == "__main__":
     cutter = Cutter()
     cutter.cut()
