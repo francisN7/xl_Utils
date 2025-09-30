9a06a6c5b53e14d15a0148882bc7fc738fbdb96b
Francis7N <francielfrancasegundo@gmail.com>
2025-09-30 09:27:55 -0300

    Iniciado verificação do tipo real de arquivo.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-from utils import set_root_dir
+from utils import FileReader, set_root_dir
 
 if __name__ == "__main__":
     set_root_dir()
+    reader = FileReader()
+    dfs = reader.run()
--- src/utils/read_files.py
+++ src/utils/read_files.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import magic
 import pandas as pd
 from file_picker_py import pick_files_blocking
 
@@ -11,7 +12,8 @@ class FileReader:
 
     def run(self) -> dict[Path, dict[str, pd.DataFrame]]:
         self.__pick_files()
-        self.__read_files()
+        self.__corret_file_type()
+        # self.__read_files()
         return self.dfs
 
     def __pick_files(self) -> None:
@@ -20,3 +22,8 @@ class FileReader:
     def __read_files(self) -> None:
         for file in self.files_paths:
             self.dfs[file] = pd.read_excel(file, dtype=str, sheet_name=None)
+
+    def __corret_file_type(self) -> None:
+        for file in self.files_paths:
+            file_type = magic.from_file(str(file), mime=True)
+            print(f"{file_type}\n")
