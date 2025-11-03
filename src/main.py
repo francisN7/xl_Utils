6a930e234019b9b1664c61b67590d1283162f55d
francisN7 <francielfrancasegundo@gmail.com>
2025-11-02 22:15:05 -0300

    Configurando entry-point para o cli-app
    Foi configurado todo o ambiente para o entry-point do cli-app futuro, além de teste inicial em main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 from core import CnjProcessor
 from utils import FileReader, save, set_root_dir
 
+
+def xl_run() -> None:
+    print("xl_run funcional!")
+
+
 if __name__ == "__main__":
     set_root_dir()
     reader = FileReader()
